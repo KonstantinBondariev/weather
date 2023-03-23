7d6d03c4ffd7ca67ac7ea0fe5ade5282e28b4c1a
Konstantin Bondariev <bondariev.konstantin@gmail.com>
2023-03-23 21:29:44 +0200

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarModule } from './modules/nav-bar/nav-bar.module';
+import { WeatherModule } from './modules/weather/weather.module';
+import { SunRadiationModule } from './modules/sun-radiation/sun-radiation.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavBarModule,
+    WeatherModule,
+    SunRadiationModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
